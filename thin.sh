11776b2c3e8826b5ff5f85da1505f531a40da465
Monica Hanson <mhanson@monicarose.tech>
2025-07-23 17:53:27 -0500

    For now just use username/password

--- thin.sh
+++ thin.sh
@@ -24,21 +24,21 @@ PROXY="172.16.200.152"
 NODE="${NODE%%\.*}"
 
 echo "Authenticating"
-# DATA="$(curl -f -s -S -k --data-urlencode "username=$USERNAME" --data-urlencode "password=$PASSWORD" "https://$PROXY:8006/api2/json/access/ticket")"
-#
-# echo "AUTH OK"
-#
-# TICKET="${DATA//\"/}"
-# TICKET="${TICKET##*ticket:}"
-# TICKET="${TICKET%%,*}"
-# TICKET="${TICKET%%\}*}"
-#
-# CSRF="${DATA//\"/}"
-# CSRF="${CSRF##*CSRFPreventionToken:}"
-# CSRF="${CSRF%%,*}"
-# CSRF="${CSRF%%\}*}"
+DATA="$(curl -f -s -S -k --data-urlencode "username=$USERNAME" --data-urlencode "password=$PASSWORD" "https://$PROXY:8006/api2/json/access/ticket")"
 
-curl -f -s -S -k -H "Authorization: PVEAPIToken=$TOKEN_ID=$SECRET" "https://$PROXY:8006/api2/spiceconfig/nodes/$NODE/qemu/$VMID/spiceproxy" -d "proxy=$PROXY" > spiceproxy
+echo "AUTH OK"
+
+TICKET="${DATA//\"/}"
+TICKET="${TICKET##*ticket:}"
+TICKET="${TICKET%%,*}"
+TICKET="${TICKET%%\}*}"
+
+CSRF="${DATA//\"/}"
+CSRF="${CSRF##*CSRFPreventionToken:}"
+CSRF="${CSRF%%,*}"
+CSRF="${CSRF%%\}*}"
+
+curl -f -s -S -k -b "PVEAuthCookie=$TICKET" -H "CSRFPreventionToken: $CSRF" "https://$PROXY:8006/api2/spiceconfig/nodes/$NODE/qemu/$VMID/spiceproxy" -d "proxy=$PROXY" > spiceproxy
 
 #Launch remote-viewer with spiceproxy file, in kiosk mode, quit on disconnect
 #The run loop will get a new ticket and launch us again if we disconnect
